839829ab5863a8c0dae6b0b113f6a780c7ae544c
SvoronaM <sergeymigal@gmail.com>
2022-10-25 21:17:18 +0200

    end

--- back/app.js
+++ back/app.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+
+mongoose.connect('mongodb+srv://OCR_1:LbwAITYsMCv16fZB@cluster0.xjpuzc3.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 app.post('/api/products', (req, res, next) => {
     const product = new Product({
